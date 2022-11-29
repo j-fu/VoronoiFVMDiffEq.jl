4081b1bfc5d8feec8e42800472351a29a9122981
Jürgen Fuhrmann <juergen-fuhrmann@web.de>
2022-11-29 11:52:05 +0100

    add documentation stuff

---
+++ docs/make.jl
@@ -0,0 +1,72 @@
+using Documenter, VoronoiFVMDiffEq, PlutoSliderServer
+using LinearAlgebra
+
+
+
+
+function make_all()
+    ENV["VORONOIFVM_CHECK_ALLOCS"]="false"
+    generated_examples=[]
+    notebook_html_dir  = joinpath(@__DIR__,"src","nbhtml")
+    #
+    # Run notebooks
+    #
+    notebooks=[
+        "1D nonlinear diffusion" => "Notebook101_NonlinearDiffusion1D.jl",
+        "1D wave equation" => "Notebook102_WaveEquation1D.jl",
+        "1D/2D brusselator" => "Notebook203_Brusselator.jl"
+    ]
+    
+    notebookjl=last.(notebooks)
+    notebookmd=[]
+    export_directory(joinpath(@__DIR__,"..","examples"),
+                     notebook_paths=notebookjl,
+                     Export_output_dir=joinpath(notebook_html_dir),
+                     Export_offer_binder=false)
+    
+    # generate frame markdown for each notebook
+    for notebook in notebookjl
+        base=split(notebook,".")[1]
+        mdstring=
+"""
+##### [$(base).jl](@id $(base))
+[Download](https://github.com/j-fu/VoronoiDiffEqFVM.jl/blob/master/examples/$(notebook))
+this [Pluto.jl](https://github.com/fonsp/Pluto.jl) notebook.
+
+```@raw html
+<iframe style="height:15000px" width="100%" src="../$(base).html"> </iframe>
+```
+"""
+        mdname=base*".md"
+        push!(notebookmd,joinpath("nbhtml",mdname))
+        io=open(joinpath(notebook_html_dir,mdname),"w")
+        write(io,mdstring)
+        close(io)
+    end     
+    
+    notebooks=first.(notebooks).=> notebookmd
+    pushfirst!(notebooks, "About the notebooks"=> "notebooks.md")
+    
+        
+    makedocs(
+        sitename="VoronoiFVM.jl",
+        modules = [VoronoiFVM],
+        clean = false, 
+        doctest = true,
+        authors = "J. Fuhrmann",
+        repo="https://github.com/j-fu/VoronoiFVM.jl",
+        pages=[ 
+            "Home"=>"index.md",
+            "Tutorial Notebooks" => notebooks
+        ]
+    )
+
+    rm(notebook_html_dir,recursive=true)
+    
+    if !isinteractive()
+        deploydocs(repo = "github.com/j-fu/VoronoiFVMDiffEq.jl.git",devbranch="main")
+    end
+end
+
+make_all()
+
---
+++ docs/src/notebooks.md
@@ -0,0 +1,8 @@
+About the notebooks
+==================
+[Pluto.jl](https://github.com/fonsp/Pluto.jl) notebooks provide a great opportunity to put together code, text and graphics 
+in a reproducible and accessible way. Therefore, the examples for this package are provided as Pluto notebooks.
+Like the example code, the notebook code is tested during CI.
+
+Plese note, that in the html version, interactive elements like sliders are disabled. Navigation via the table of contents does work, though.
+
